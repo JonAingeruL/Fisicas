d2dc0776acdbd1887680bd018fa7339c5a601815
JonAingeruL <j.lobato@opendeusto.es>
2025-03-21 09:44:28 +0100

    MorePrefabs

--- Assets/Scripts/Planes/planeMovement.cs
+++ Assets/Scripts/Planes/planeMovement.cs
@@ -1,12 +1,29 @@
 using UnityEngine;
 
-public class MovimientoRecto : MonoBehaviour
+public class PlaneMovement : MonoBehaviour
 {
-    public float velocidad = 10f; // Velocidad del movimiento
+    public float velocidad = 5f;  // Velocidad de movimiento
+    public float alturaFija = 5f; // Altura en la que se mantiene el objeto
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false; // Desactivar gravedad
+        rb.constraints = RigidbodyConstraints.FreezePositionY; // Congelar en altura
+    }
 
     void Update()
     {
-        transform.position -= Vector3.right * velocidad * Time.deltaTime;
+        // Mover en el eje X (hacia la derecha)
+        rb.velocity = new Vector3(-velocidad, 0, 0);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("¡Colisión con: " + other.gameObject.name);
     }
 }
+
 
